5f527d136d6ddf4293c6c0cc4be00f86acc01d4d
VetleViking <vetlelind@gmail.com>
2024-01-09 20:39:37 +0100

    Add search functionality to HTML pages

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,21 @@ function createListItem(element) {
     document.getElementById("allEquipment").appendChild(html);
     return html;
 }
+
+function filterSearch() {
+    let input = document.getElementById("searchInput").value;
+    input = input.toLowerCase();
+
+    let all = document.getElementsByClassName("listDiv");
+
+    for (let i = 0; i < all.length; i++) {
+        let text = all[i].innerText;
+        text = text.toLowerCase();
+
+        if (text.includes(input)) {
+            all[i].style.display = "grid";
+        } else {
+            all[i].style.display = "none";
+        }
+    }
+}
